ed67fe33fe605081f295c27b34e3cfda65322c49
LeoNozaki <leosnozaki@gmail.com>
2020-11-09 10:05:01 -0300

    atualizando

---
+++ src/components/MeuBotao.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="incremento">{{ texto }}</button>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      contador: 0
+    }
+  },
+  computed: {
+    vezes () {
+      return this.contador === 1 ? 'vez' : 'vezes'
+    },
+    texto () {
+      return `Clicado ${this.contador} ${this.vezes}`
+    }
+  },
+  methods: {
+    incremento () {
+      this.contador += 1
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import botaoSimples from './components/MeuBotao.vue'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export default {
+  install (Vue) {
+    Vue.component('Simplebutton', botaoSimples)
+  }
+}
